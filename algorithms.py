6620a1899052e1f2f925e31a037b39134ec210a1
adrianaavilees <adrianaaviles0606@gmail.com>
2024-12-13 15:58:36 +0100

    plot updated

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,7 @@ FUNCIÓ PRINCIPAL:
 Aquest script serveix per descriure els diferents algoritmes per al nostre model. 
 A més d'implementar diferents metodes per visualitzar els nostres resultats. 
 """
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -18,6 +19,7 @@ import numpy as np
 from sklearn.metrics import roc_curve, auc
 from sklearn.preprocessing import label_binarize
 
+
 class Models:
     def __init__(self, X_train, y_train, X_test, y_test) -> None:
         self._X_train = X_train
@@ -25,99 +27,138 @@ class Models:
         self._X_test = X_test
         self._y_test = y_test
 
-        self._prediccions = {}  #? {nom_model : predict}
-        self._metrics = []  # emmagatzema les mètriques de RENDIMENT dels models, per fer el dataframe
-        self._cache = 'cache_data'
+        self._prediccions = {}  # ? {nom_model : predict}
+        self._metrics = (
+            []
+        )  # emmagatzema les mètriques de RENDIMENT dels models, per fer el dataframe
+        self._cache = "cache_data"
 
     ######### SAVE
-    def create_metrics_dataframe(self, filename='metrics.csv') -> pd.DataFrame:
+    def create_metrics_dataframe(self, filename="metrics.csv") -> pd.DataFrame:
         """Crear un DataFrame amb les mètriques per a cada model.
-        També crea un csv """
-        #* Crear el DataFrame
+        També crea un csv"""
+        # * Crear el DataFrame
         metrics_df = pd.DataFrame(self._metrics)
-        
-        #* Desar el DataFrame en un fitxer CSV
+
+        # * Desar el DataFrame en un fitxer CSV
         metrics_df.to_csv(filename, index=False)
         print(f"Mètriques desades a {filename}")
-        
+
         return metrics_df
-    
-    def save_model(self, model_name:str, model:object, filename:str):
+
+    def save_model(self, model_name: str, model: object, filename: str):
         """ENTRENAR i GUARDAR el model si no existeix."""
         # Comprovar si el model ja existeix com a pickle
         if os.path.exists(filename):
             print(f"El model {model_name} ja està entrenat i guardat.")
-            with open(filename, 'rb') as f:
+            with open(filename, "rb") as f:
                 model = pickle.load(f)
         else:
             print(f"Entrenant el model {model_name}...")
             model.fit(self._X_train, self._y_train)
-            with open(filename, 'wb') as f:
+            with open(filename, "wb") as f:
                 pickle.dump(model, f)
             print(f"Model {model_name} entrenat i guardat com a pickle.")
 
         y_pred = model.predict(self._X_test)
         self._prediccions[model_name] = y_pred
-    
 
     ######### ALGORISMES - MODELS
-    def do_knn(self, dataset_name:str, n_neighbors=5, weights='uniform', algorithm='auto'):
-        knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, algorithm=algorithm)
-        filename = os.path.join(self._cache, f'KNN_{dataset_name}.pkl')
-        self.save_model('KNN', knn, filename)
+    def do_knn(
+        self, dataset_name: str, n_neighbors=5, weights="uniform", algorithm="auto"
+    ):
+        knn = KNeighborsClassifier(
+            n_neighbors=n_neighbors, weights=weights, algorithm=algorithm
+        )
+        filename = os.path.join(self._cache, f"KNN_{dataset_name}.pkl")
+        self.save_model("KNN", knn, filename)
 
-    def do_svm(self, dataset_name:str, kernel='rbf', C=1.0, gamma='scale', random_state=None):
+    def do_svm(
+        self, dataset_name: str, kernel="rbf", C=1.0, gamma="scale", random_state=None
+    ):
         svm = SVC(kernel=kernel, C=C, gamma=gamma, random_state=random_state)
-        filename = os.path.join(self._cache, f'SVM_{dataset_name}.pkl')
-        self.save_model('SVM', svm, filename)
+        filename = os.path.join(self._cache, f"SVM_{dataset_name}.pkl")
+        self.save_model("SVM", svm, filename)
 
-    def do_decision_tree(self, dataset_name:str, random_state=42):
+    def do_decision_tree(self, dataset_name: str, random_state=42):
         dtree = DecisionTreeClassifier(random_state=random_state)
-        filename = os.path.join(self._cache, f'DecisionTree_{dataset_name}.pkl')
-        self.save_model('Decision Tree', dtree, filename)
-    
-    def do_random_forest(self, dataset_name:str, n_estimators=100, random_state=42):
-        random_forest = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
-        filename = os.path.join(self._cache, f'RandomForest_{dataset_name}.pkl')
-        self.save_model('Random Forest', random_forest, filename)
+        filename = os.path.join(self._cache, f"DecisionTree_{dataset_name}.pkl")
+        self.save_model("Decision Tree", dtree, filename)
 
-    def do_gradient_boosting(self, dataset_name:str, learning_rate=0.1, n_estimators=100, random_state=42):
-        gb = GradientBoostingClassifier(learning_rate=learning_rate, n_estimators=n_estimators, random_state=random_state)
-        filename = os.path.join(self._cache, f'GradientBoosting_{dataset_name}.pkl')
-        self.save_model('Gradient Boosting', gb, filename)
+    def do_random_forest(self, dataset_name: str, n_estimators=100, random_state=42):
+        random_forest = RandomForestClassifier(
+            n_estimators=n_estimators, random_state=random_state
+        )
+        filename = os.path.join(self._cache, f"RandomForest_{dataset_name}.pkl")
+        self.save_model("Random Forest", random_forest, filename)
 
-    def do_logistic_regression(self, dataset_name:str, C=1.0,solver='liblinear', max_iter=5000, penalty='l2', random_state=42):
-        logistic_regression = LogisticRegression(C=1, solver='liblinear', max_iter=5000, penalty='l2', random_state=42)
-        filename = os.path.join(self._cache, f'LogisticRegression_{dataset_name}.pkl')
-        self.save_model('Logistic Regression', logistic_regression, filename)
-    
-    def do_gaussian_naive_bayes(self, dataset_name:str):
+    def do_gradient_boosting(
+        self, dataset_name: str, learning_rate=0.1, n_estimators=100, random_state=42
+    ):
+        gb = GradientBoostingClassifier(
+            learning_rate=learning_rate,
+            n_estimators=n_estimators,
+            random_state=random_state,
+        )
+        filename = os.path.join(self._cache, f"GradientBoosting_{dataset_name}.pkl")
+        self.save_model("Gradient Boosting", gb, filename)
+
+    def do_logistic_regression(
+        self,
+        dataset_name: str,
+        C=1.0,
+        solver="liblinear",
+        max_iter=5000,
+        penalty="l2",
+        random_state=42,
+    ):
+        logistic_regression = LogisticRegression(
+            C=1, solver="liblinear", max_iter=5000, penalty="l2", random_state=42
+        )
+        filename = os.path.join(self._cache, f"LogisticRegression_{dataset_name}.pkl")
+        self.save_model("Logistic Regression", logistic_regression, filename)
+
+    def do_gaussian_naive_bayes(self, dataset_name: str):
         gnb = GaussianNB()
-        filename = os.path.join(self._cache, f'GaussianNB_{dataset_name}.pkl')
-        self.save_model('Gaussian NB', gnb, filename)
+        filename = os.path.join(self._cache, f"GaussianNB_{dataset_name}.pkl")
+        self.save_model("Gaussian NB", gnb, filename)
 
-    def do_bernoulli_naive_bayes(self, dataset_name:str, alpha=1.0, binarize=0):
+    def do_bernoulli_naive_bayes(self, dataset_name: str, alpha=1.0, binarize=0):
         bnb = BernoulliNB(alpha=alpha, binarize=binarize)
-        filename = os.path.join(self._cache, f'BernoulliNB_{dataset_name}.pkl')
-        self.save_model('Bernoulli NB', bnb, filename)
+        filename = os.path.join(self._cache, f"BernoulliNB_{dataset_name}.pkl")
+        self.save_model("Bernoulli NB", bnb, filename)
 
     def do_multinomial_nb(self, dataset_name: str, alpha=1.0):
         multinomial_nb = MultinomialNB(alpha=alpha)
-        filename = os.path.join(self._cache, f'MultinomialNB_{dataset_name}.pkl')
-        self.save_model('Multinomial NB', multinomial_nb, filename)
-
+        filename = os.path.join(self._cache, f"MultinomialNB_{dataset_name}.pkl")
+        self.save_model("Multinomial NB", multinomial_nb, filename)
 
     ######## METRIQUES
-    def do_confusion_matrix(self, cm:object, model_name:str, dataset_name:str, labels:list, dir='confusion_matrixs', show=False):
+    def do_confusion_matrix(
+        self,
+        cm: object,
+        model_name: str,
+        dataset_name: str,
+        labels: list,
+        dir="confusion_matrixs",
+        show=False,
+    ):
         """Visualitzar la matriu de confusió."""
         plt.figure(figsize=(8, 6))
-        
+
         # Definim els "eixos" amb NOM dels labels
         if labels:
-            sns.heatmap(cm, annot=True, fmt=".2f", cmap="Blues", xticklabels=labels, yticklabels=labels)
+            sns.heatmap(
+                cm,
+                annot=True,
+                fmt=".2f",
+                cmap="Blues",
+                xticklabels=labels,
+                yticklabels=labels,
+            )
         else:
             sns.heatmap(cm, annot=True, fmt=".2f", cmap="Blues")
-        
+
         plt.title(f"Matriu de Confusió - {model_name} - {dataset_name}")
         plt.xlabel("Predicció")
         plt.ylabel("Realitat")
@@ -125,43 +166,91 @@ class Models:
         # Si el directori no existeix, es crea
         if not os.path.exists(dir):
             os.makedirs(dir)
-        
-        output_filename = os.path.join(dir, f"{model_name}_{dataset_name}_confusion_matrix.png")
+
+        output_filename = os.path.join(
+            dir, f"{model_name}_{dataset_name}_confusion_matrix.png"
+        )
         plt.savefig(output_filename)
         print(f"Matriu de confusió desada a {output_filename}")
 
         if show:
             plt.show()
 
-    def evaluate_model(self, model_name:str, dataset_name:str, labels:list):
+    def evaluate_model(self, model_name: str, dataset_name: str, labels: list):
         """Avalua un model determinat i l'afegeix a la llista per crear posteriorment el dataset"""
         if model_name not in self._prediccions:
             raise ValueError(f"El model '{model_name}' no es troba en el diccionari.")
-        
+
         prediction = self._prediccions[model_name]
 
         # Avaluem el model amb les metriques
         accuracy = accuracy_score(self._y_test, prediction)
         precision = precision_score(self._y_test, prediction, average="weighted")
         f1 = f1_score(self._y_test, prediction, average="weighted")
-        cm = confusion_matrix(self._y_test, prediction, normalize='true')
+        cm = confusion_matrix(self._y_test, prediction, normalize="true")
 
         # Visualitzar la matriu de confusió
         self.do_confusion_matrix(cm, model_name, dataset_name, labels)
 
         # Afegir resultats al registre
-        self._metrics.append({
-            "Algorisme": model_name,
-            "Dataset": dataset_name,
-            "Accuracy": accuracy,
-            "Precision": precision,
-            "F1-Score": f1
-        })
+        self._metrics.append(
+            {
+                "Algorisme": model_name,
+                "Dataset": dataset_name,
+                "Accuracy": accuracy,
+                "Precision": precision,
+                "F1-Score": f1,
+            }
+        )
+
+    def do_plot_metrics(self, metrics_filename, show=True):
+        metrics_df = pd.read_csv(metrics_filename)
+        models = metrics_df["Algorisme"]
+        metrics = metrics_df[["Accuracy", "Precision", "F1-Score"]]
+
+        # Crear el grafic de barres
+        fig, ax = plt.subplots(figsize=(10, 6))
+        n_models = len(models)
+        n_metrics = len(metrics.columns)
+
+        # Configrar les posicions de les barres
+        bar_width = 0.1
+        x = range(n_models)
+
+        colors = ["pink", "lightgreen", "lightblue"]  # Colors per cada metrica
+
+        # Dibuixem les barres per cada metrica
+        for idx, metric in enumerate(metrics.columns):
+            bar_positions = [
+                pos + idx * bar_width for pos in x
+            ]  # Desplazar les posicions pq quadri
+            ax.bar(
+                bar_positions,
+                metrics[metric],
+                width=bar_width,
+                label=metric,
+                color=colors[idx],
+            )
+
+        # Ajustar les posicions en l'eix de les X perque esten centrades
+        ax.set_xticks([pos + bar_width * (n_metrics - 1) / 2 for pos in x])
+        ax.set_xticklabels(models)
+
+        ax.set_title("Comparació de Metriques dels Models", fontsize=16)
+        ax.set_xlabel("Models", fontsize=14)
+        ax.set_ylabel("Valor de Metriques", fontsize=14)
+        ax.legend(title="Metriques", fontsize=12)
+        ax.grid(axis="y", linestyle="--", alpha=0.7)
+
+        plt.tight_layout()
+
+        if show:
+            plt.show()
 
     def generate_roc_curve(model, X_test, y_test, model_name, output_path="roc_curves"):
         """Genera y guarda la curva ROC para un modelo de clasificación.
         Compatible con modelos que tienen o no 'predict_proba'."""
-        
+
         # Verificar si las etiquetas son continuas
         if np.issubdtype(np.array(y_test).dtype, np.floating):
             print("Error: Las etiquetas son continuas. ROC no es aplicable.")
@@ -175,7 +264,9 @@ class Models:
         try:
             y_prob = model.predict_proba(X_test)
         except AttributeError:
-            print(f"{model_name} no tiene 'predict_proba'. Usando 'predict' en su lugar.")
+            print(
+                f"{model_name} no tiene 'predict_proba'. Usando 'predict' en su lugar."
+            )
             y_prob = model.predict(X_test)
             y_prob = np.expand_dims(y_prob, axis=1)
 
@@ -183,19 +274,27 @@ class Models:
         tpr = dict()
         roc_auc = dict()
 
-        if y_prob.ndim > 1:  
+        if y_prob.ndim > 1:
             for i in range(n_classes):
                 fpr[i], tpr[i], _ = roc_curve(y_test_binarized[:, i], y_prob[:, i])
                 roc_auc[i] = auc(fpr[i], tpr[i])
 
-            fpr_micro, tpr_micro, _ = roc_curve(y_test_binarized.ravel(), y_prob.ravel())
+            fpr_micro, tpr_micro, _ = roc_curve(
+                y_test_binarized.ravel(), y_prob.ravel()
+            )
             roc_auc_micro = auc(fpr_micro, tpr_micro)
 
             plt.figure(figsize=(10, 8))
             for i in range(n_classes):
                 plt.plot(fpr[i], tpr[i], label=f"Clase {i} (AUC = {roc_auc[i]:.2f})")
-            
-            plt.plot(fpr_micro, tpr_micro, label=f"Micro promedio (AUC = {roc_auc_micro:.2f})", linestyle='--', color='red')
+
+            plt.plot(
+                fpr_micro,
+                tpr_micro,
+                label=f"Micro promedio (AUC = {roc_auc_micro:.2f})",
+                linestyle="--",
+                color="red",
+            )
 
         else:
             fpr[0], tpr[0], _ = roc_curve(y_test_binarized[:, 0], y_prob)
@@ -203,8 +302,8 @@ class Models:
 
             plt.figure(figsize=(10, 8))
             plt.plot(fpr[0], tpr[0], label=f"Clase 0 (AUC = {roc_auc[0]:.2f})")
-        
-        plt.plot([0, 1], [0, 1], 'k--', label="Aleatoria (AUC = 0.5)")
+
+        plt.plot([0, 1], [0, 1], "k--", label="Aleatoria (AUC = 0.5)")
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
         plt.xlabel("Tasa de Falsos Positivos (FPR)")
@@ -213,7 +312,7 @@ class Models:
         plt.legend(loc="lower right")
 
         output_file = os.path.join(output_path, f"roc_curve_{model_name}.pkl")
-        with open(output_file, 'wb') as f:
+        with open(output_file, "wb") as f:
             pickle.dump(plt, f)
         plt.close()
 
