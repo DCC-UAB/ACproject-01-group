ec8ac84be3c2b51460772bf6888f0bb1bd2e16e1
adrianaavilees <adrianaaviles0606@gmail.com>
2024-12-13 16:17:47 +0100

    images

--- main.py
+++ main.py
@@ -75,18 +75,19 @@ def main():
         f"IMAGES carregades. Train shape: {dataIMG.train_data.shape}, Test shape: {dataIMG.test_data.shape}"
     )
 
-    print("\n--- IMPLEMENTAR MODELS ---")
+    print("\n--- IMPLEMENTAR MODELS AMB CSV ---")
     models3 = Models(
         data3.train_data, data3.train_labels, data3.test_data, data3.test_labels
     )
-    dataset_name = "df_3s"
-    MODELS3_DICT = get_models(models3)
-    LABELS = data3.get_labels()
+    #! DO MODELS AMB IMATGES
+    # dataset_name = "df_3s"
+    # MODELS3_DICT = get_models(models3)
+    # LABELS = data3.get_labels()
 
-    # * Entrenem cada model
-    for model_str, model_train in MODELS3_DICT.items():
-        model_train(dataset_name)
-        models3.evaluate_model(model_str, dataset_name, LABELS)
+    # # * Entrenem cada model
+    # for model_str, model_train in MODELS3_DICT.items():
+    #     model_train(dataset_name)
+    #     models3.evaluate_model(model_str, dataset_name, LABELS)
 
     metrics3_df = models3.create_metrics_dataframe()
     # models3.do_plot_metrics('metrics.csv')
@@ -103,6 +104,23 @@ def main():
 
     metrics30_df = models30.create_metrics_dataframe()
     # models30.do_plot_metrics('metrics.csv')
+
+    print("\n--- IMPLEMENTAR MODELS AMB IMATGES ---")
+    modelsIMG = Models(
+        dataIMG.train_data, dataIMG.train_labels, dataIMG.test_data, dataIMG.test_labels
+    )
+    dataset_name = "images"
+    MODELSIMG_DICT = get_models(modelsIMG)
+    LABELS = dataIMG.get_labels()
+
+    # Entrenar cada modelo con imágenes
+    for model_str, model_train in MODELSIMG_DICT.items():
+        print(f"Entrenant el model {model_str} amb les imatges...")
+        model_train(dataset_name)
+        modelsIMG.evaluate_model(model_str, dataset_name, LABELS)
+
+    metricsIMG_df = modelsIMG.create_metrics_dataframe("metrics_images.csv")
+    print(f"Mètriques dels models amb imatges desades a 'metrics_images.csv'")
 
     # * FEM UN MERGE PER TENIR UN CSV UNIC DE LES DADES DE 3 I 30S
     merged_df = pd.concat([metrics3_df, metrics30_df], ignore_index=True)
